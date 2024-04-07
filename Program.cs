e7cf7d33932ff59dc4fc6616cd8467d778408165
Albinotektek <162760294+Albinotektek@users.noreply.github.com>
2024-04-07 19:34:59 -0300

    soletrando

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine();
+
+Console.WriteLine("----Soletrar----\n");
+
+Console.Write("Digite uma palavra:");
+string txt = Console.ReadLine()!;
+
+Console.WriteLine();
+
+Console.WriteLine("\nSoletrando fica:\n");
+
+Console.WriteLine(string.Join("-",txt.ToUpper().ToCharArray()));
+
+Console.WriteLine();
